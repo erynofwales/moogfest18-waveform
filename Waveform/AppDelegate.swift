1b4a17dc20f1e52965e4a5ba9b28d469b45ccd04
Eryn Wells <eryn@erynwells.me>
2018-09-16 22:04:00 -0700

    Initial Commit

---
+++ Waveform/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Waveform
+//
+//  Created by Eryn Wells on 9/16/18.
+//  Copyright © 2018 Eryn Wells. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
